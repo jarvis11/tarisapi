a6af475230eff767736896526481be239ae67f75
jarvis11 <bvravi11@gmail.com>
2014-11-14 15:01:16 -0600

    Mediabuyer Documentation
    Documentation of mediabuyer routes complete. Still need to document
    venue routes and all models.

--- routes/mediabuyer_routes.js
+++ routes/mediabuyer_routes.js
@@ -1,3 +1,17 @@
+/*===============================================================================================================
+
+Mediabuyer routes allow you to manipulate medaibuyer objects
+Mediabuyer objects own campiagn, and subsequently ad objects
+Mediabuyer objects are stored in their own collection, referencing seperate collections of campaigns
+and ads for efficiency.
+
+=================================================================================================================*/
+
+
+
+
+
+
 //initialize Express router
 var express = require('express');
 var router = express.Router();
@@ -14,35 +28,45 @@ var Ad = require('../models/campaign_document/ad');
 
 ROUTES GO HERE
 
+All routes are accessed at: http://hosturl/api/...
+
 =============================================================================================*/
 
-//high level mediabuyer routes (GET, POST)
+
+/*========================================================================
+
+Query all mediabuyer objects (GET)
+Create a new mediabuyer object (POST)
+
+==========================================================================*/
+
 router.route('/mediabuyers')
 
-	//Query all mediabuyers
-	//Accessed at: GET http://localhost:8080/api/mediabuyers
 	.get(function(req, res){
 
 		//find array of all media buyers
 		Mediabuyer.find(function(err, mediabuyers){
-			if(err)
+			if(err){
+				console.log('error getting mediabuyers');
 				res.send(err);
-			console.log('getting all mediabuyers');
-
-			//return array of mediabuyers
-			res.json(mediabuyers);
+			} else {
+				//return array of mediabuyers
+				console.log('getting all mediabuyers');
+				res.json(mediabuyers);
+			}
 		});
 	})
 
-	//Add a new mediabuyer to an account
-	//Accessed at: POST http://localhost:8080/api/mediabuyers
+
 	.post(function(req,res){
 
-		//create a new media buyer
+		//create a new mediabuyer object
 		var mediabuyer = new Mediabuyer();
+
+		//assign attributes
 		mediabuyer.name = req.body.name;
 
-		//save our new mediabuyer
+		//save mediabuyer object
 		mediabuyer.save(function(err){
 
 			//ensure parameters are valid
@@ -52,19 +76,23 @@ router.route('/mediabuyers')
 			} else{
 				console.log('adding a new mediabuyer');
 				res.json({message: 'You have created a mediabuyer!'});
-
 			}
-			
 		});
 	});
 
 
-//routes for specific mediabuyer (GET, PUT)
+/*========================================================================
+
+Query a specific mediabuyer object (GET)
+Edit an existing mediabuyer (PUT)
+
+==========================================================================*/
+
 router.route('/mediabuyers/:mediabuyer_id')
 
-	//Get a mediabuyer with id :mediabuyer_id
-	//Accessed at: GET http://localhost:8080/api/mediabuyers/:mediabuyer_id
 	.get(function(req, res) {
+
+		//find mediabuyer through inputed id :mediabuyer_id
 		Mediabuyer.findById(req.params.mediabuyer_id, function(err, mediabuyer) {
 
 			//ensure id is valid
@@ -72,6 +100,8 @@ router.route('/mediabuyers/:mediabuyer_id')
 				console.log('error getting mediabuyer -- invalid id');
 				res.send(err);
 			} else{
+
+				//return mediabuyer object
 				console.log('getting mediabuyer with id ' + req.params.mediabuyer_id);
 				res.json(mediabuyer);
 			}
@@ -79,14 +109,13 @@ router.route('/mediabuyers/:mediabuyer_id')
 		});
 	})
 
-	//Update a mediabuyer's core attributes
-	//Accessed at: PUT http://localhost:8080/api/mediabuyers/:mediabuyer_id
+
 	.put(function(req, res) {
 
-		//Find the mediabuyer we want
+		//find mediabuyer through inputed id :mediabuyer_id
 		Mediabuyer.findById(req.params.mediabuyer_id, function(err, mediabuyer) {
 
-			//ensure mediabuyer id is valid
+			//ensure id is valid
 			if (err){
 				console.log('error getting mediabuyer -- invalid id');
 				res.send(err);
@@ -95,31 +124,37 @@ router.route('/mediabuyers/:mediabuyer_id')
 				//update mediabuyer attributes
 				mediabuyer.name = req.body.name; 
 
-				//save the Mediabuyer
+				//save mediabuyer object
 				mediabuyer.save(function(err) {
 
 					//ensure parameters are valid
 					if (err){
+						console.log('error -- invalid parameters')
 						res.send(err);
 					} else {
 						console.log('updating a specific mediabuyer');
 						res.json({message: 'Mediabuyer has been updated.'});
-					} // close mediabuyer SAVE error check
+					}
 				});
-
-			} // close mediabuyer GET error check
-
+			}
 		});
 	});
 
-//routes for finding campaigns associated with a specific mediabuyer (GET, POST)
+
+/*========================================================================
+
+Query all campaigns belonging to a mediabuyer (GET)
+Create a new campaign for a specific mediabuyer (POST)
+
+==========================================================================*/
+
 router.route('/mediabuyers/:mediabuyer_id/campaigns')	
 	
-	//Find all campaigns belonging to a mediabuyer
-	//Accessed at: GET http://localhost:8080/mediabuyers/:mediabuyer_id/campaigns
 	.get(function(req, res) {
 
-		//populate the campaigns attribute of our mediabuyer object using the campaigns collection
+		//MONGOOSE POPULATE
+		//utilize mongoose's populate method to connect mediabuyer and campaigns collection
+		//populate the campaigns attribute of mediabuyer object through the campaigns collection
 		Mediabuyer.findById(req.params.mediabuyer_id).populate('campaigns').exec(function(err, mediabuyer){
 
 			//ensure mediabuyer id is valid
@@ -137,13 +172,12 @@ router.route('/mediabuyers/:mediabuyer_id/campaigns')
 	})
 
 
-	//Add a campaign to a mediabuyers account
-	//Accessed at: POST http://localhost:8080/api/mediabuyers/:mediabuyer_id/campaigns
 	.post(function(req, res) {
 
+		//find mediabuyer through inputed id :mediabuyer_id
 		Mediabuyer.findById(req.params.mediabuyer_id, function(err, mediabuyer) {
 
-			//ensure mediabuyer id is valid
+			//ensure :mediabuyer_id is valid
 			if (err){
 				console.log('error getting mediabuyer -- invalid id');
 				res.send(err);
@@ -155,7 +189,7 @@ router.route('/mediabuyers/:mediabuyer_id/campaigns')
 				campaign.type = req.body.type;
 				campaign.budget = req.body.budget;
 
-				//validate your campaign
+				//save campaign
 				campaign.save(function(err){
 
 					//ensure campaign parameters are valid
@@ -163,35 +197,39 @@ router.route('/mediabuyers/:mediabuyer_id/campaigns')
 						res.send(err);
 					}
 					else{
+
 						//if campaign has been validated push it to mediabuyer
 						mediabuyer.campaigns.push(campaign);
 
 						//save mediabuyer object
+						//no need to save campaign object seperately, as it belongs to the mediabuyer
 						mediabuyer.save(function(err){
 							if(err)
 								res.send(err);
 							res.json({message: 'You have added a campaign to this mediabuyer'});
-
 						});
-
-					} //close campaign parameter error check
-
+					}
 				});
-
-			} //close mediabuyer GET error check
-
+			}
 		});
-
 	});
 
 
-//routes associated with single campaign objects (GET, PUT)
+/*========================================================================
+
+Query a specific campaign belonging to a mediabuyer (GET)
+Edit an existing campaign (PUT)
+
+==========================================================================*/
+
+
 router.route('/mediabuyers/:mediabuyer_id/campaigns/:campaign_id')
 
 	.get(function(req, res){
 
 		//Find mediabuyer with id :mediabuyer_id
-		//Populate its campaigns attr with a single campaign with id :campaign_id
+		//Populate its campaigns attribute with a single campaign with id :campaign_id
+		//This will still return an array of campiagns, but the array will only contain the desired campaign
 		Mediabuyer.findById(req.params.mediabuyer_id).populate({
 			
 			path: 'campaigns', 
@@ -199,28 +237,27 @@ router.route('/mediabuyers/:mediabuyer_id/campaigns/:campaign_id')
 
 		}).exec(function(err, mediabuyer){
 
-			//ensure a mediabuyer entity exists
+			//ensure :mediabuyer_id is valid
 			if(err){
 				res.send(err);
 				console.log("error -- invalid campaign or mediabuyer id");
 			} else{
 
+				//grab the actual campiagn object out of the populated array
 				var campaign = mediabuyer.campaigns[0];
 
 				//ensure campaign has been created
+				//if mediabuyer has no campaigns created, the returned object could be null
+				//population would have no effect in this scenario, but embedded objects are still accessible
 				if(campaign == undefined){
 					console.log('ERROR');
 					res.json({message: 'error -- invalid campaign id'});
 
 				} else{
-
 					//return campaign
 					res.json(campaign);
 				}
-
-
 			} 
-			
 		});
 	})
 
@@ -241,49 +278,56 @@ router.route('/mediabuyers/:mediabuyer_id/campaigns/:campaign_id')
 				res.send(err);
 			} else{
 
-				//Grab single campaign object
+				//grab the actual campiagn object out of the populated array
 				campaign = mediabuyer.campaigns[0];
 
-				//ensure object is valid
+				//ensure campaign has been created
+				//if mediabuyer has no campaigns created, the returned object could be null
+				//population would have no effect in this scenario, but embedded objects are still accessible
 				if(campaign == undefined){
 					console.log('ERROR');
 					res.json({message: 'error -- invalid campaign id'});
 				} else{
 
-					//TYPE IS NOT EDITABLE -- MAKE NOTE IN API
+					//edit attributes of campiagn
+					//type attribute is not editable -- new campiagn should be created
 					campaign.name = req.body.name; 	
 					campaign.budget = req.body.budget;
 					campaign.status = req.body.status;
 
-					//because mediabuyer has already pushed campaign, we only need to save the campaign in question!
+					//because mediabuyer has already pushed campaign, we only need to save the campaign in question, not the mediabuyer
 					campaign.save(function(err) {
 
-						//ensure valid parameters
+						//ensure attributes are valid
 						if (err){
+							console.log('error updating campiagn -- invalid attributes');
 							res.send(err);
 						} else{
 
 							console.log('updating a specific campaign');
 							res.json({message: 'Campaign has been updated.'});
-
 						}
-						
 					});
-
 				}
-
 			}
-
 		});
-
 	});
 
 
-//routes associated with ads belonging to a campaign
+/*========================================================================
+
+Query all ads belonging to a campaign (GET)
+Create an ad under a specific campaign (POST)
+
+==========================================================================*/
+
+
 router.route('/mediabuyers/:mediabuyer_id/campaigns/:campaign_id/ads')
 
 	.get(function(req, res){
 
+		//Find mediabuyer by id :mediabuyer_id
+		//Populate campiagn with id :campaign_id belonging to mediabuyer in question
 		Mediabuyer.findById(req.params.mediabuyer_id).populate({
 			
 			path: 'campaigns', 
@@ -300,28 +344,30 @@ router.route('/mediabuyers/:mediabuyer_id/campaigns/:campaign_id/ads')
 				//return campaign with specific id
 				console.log('getting campaign with id ' + req.params.campaign_id);
 
-				//grab campaign object
+				//grab the actual campiagn object out of the populated array
 				campaign = mediabuyer.campaigns[0];
 
-				//ensure object exists
+				//ensure campaign with id :campiagn_id exists
+				//if mediabuyer has no campaigns created, the returned object could be null
+				//population would have no effect in this scenario, but embedded objects are still accessible
 				if(campaign == undefined){
 					console.log('ERROR');
 					res.json({message: 'error -- invalid campaign id'});
 				} else{
-					//return ads
+
+					//if campiagn exists, return ads belonging to campaign
 					console.log('returning all ads for campaign with id ' + req.params.campaign_id);
 					res.json(campaign.ads);
 				}				
-
 			}
-			
 		});
 	})
 
 
 	.post(function(req, res){
 
-		//Find the mediabuyer
+		//find mediabuyer with id :mediabuyer_id
+		//populate campaign with id :campaign_id
 		Mediabuyer.findById(req.params.mediabuyer_id).populate({
 
 			path: 'campaigns',
@@ -329,15 +375,18 @@ router.route('/mediabuyers/:mediabuyer_id/campaigns/:campaign_id/ads')
 
 		}).exec(function(err, mediabuyer){
 
+			//ensure :mediabuyer_id is valid
 			if(err){
 				console.log("invalid mediabuyer or campaign id");
 				res.send(err);
 			} else{
 
-				//find campaign
+				//grab the actual campiagn object out of the populated array
 				campaign = mediabuyer.campaigns[0];
 
-				//make sure campaign exists
+				//ensure campaign with id :campiagn_id exists
+				//if mediabuyer has no campaigns created, the returned object could be null
+				//population would have no effect in this scenario, but embedded objects are still accessible
 				if(campaign == undefined){
 					console.log('error -- undefined campaign');
 					res.json({message: 'error -- invalid campaign id'});
@@ -347,8 +396,7 @@ router.route('/mediabuyers/:mediabuyer_id/campaigns/:campaign_id/ads')
 					var ad = new Ad();
 					ad.type = req.body.type;
 
-					//set bidding values
-					//DEFAULT BID AMOUNT IS 5 -- NOTE IN API DOCS
+					//set bidding attributes
 					ad.bid.bidtype = req.body.bidtype;
 					ad.bid.amount = req.body.amount;
 
@@ -364,37 +412,38 @@ router.route('/mediabuyers/:mediabuyer_id/campaigns/:campaign_id/ads')
 					//push our ad
 					campaign.ads.push(ad);
 
-					//because mediabuyer has already pushed campaign, we only need to save the campaign in question!
-
+					//because mediabuyer has already pushed campaign, we only need to save the campaign in question
 					campaign.save(function(err) {
+
+						//ensure validity of ad attributes
 						if (err){
 							res.send(err);
 							console.log('error creating ad');
 						} else {
-
 							console.log('adding a new ad to this campaign');
 							res.json({message: 'Added a new ad to this campaign.'});
 						}
-						
 					});
-
-
 				}
-
 			}
-
-			
 		});
 	});
 
 
+/*========================================================================
+
+Query a specific ad belonging to a campaign (GET)
+Edit an existing ad (POST)
+
+==========================================================================*/
 
 
 router.route('/mediabuyers/:mediabuyer_id/campaigns/:campaign_id/ads/:ad_id')
 
 	.get(function(req, res){
 
-		//Find desired mediabuyer, populating only campaign with id :campaign_id
+		//find mediabuyer with id :mediabuyer_id
+		//populate campaign with id :campaign_id
 		Mediabuyer.findById(req.params.mediabuyer_id).populate({
 			
 			path: 'campaigns', 
@@ -402,29 +451,36 @@ router.route('/mediabuyers/:mediabuyer_id/campaigns/:campaign_id/ads/:ad_id')
 
 		}).exec(function(err, mediabuyer){
 
-			//THIS IF IS NOT NEEDED AS THE POPULATION ONLY PERTAINS TO CAMPAIGNS
+			//ensure :mediabuyer_id is valid
 			if(err){
 				res.send(err);
 				console.log("Not a valid mediabuyer, campaign, or ad id");
 			} else {
 
-				//store our campaign
+				//grab the actual campiagn object out of the populated array
 				campaign = mediabuyer.campaigns[0];
+
+				//ensure campaign with id :campiagn_id exists
+				//if mediabuyer has no campaigns created, the returned object could be null
+				//population would have no effect in this scenario, but embedded objects are still accessible
 				if (campaign == undefined){
 					console.log('error -- undefined campaign');
 					res.json({message: 'error -- invalid campaign id'});
 				} else{
 
 					//WORTH ADDING AN ERROR CHECK FOR AN AD HERE?
+					var ad = campaign.ads.id(req.params.ad_id);
 
-					//return campaign with specific id
-					console.log('getting ad with id ' + req.params.ad_id);
-					res.json(campaign.ads.id(req.params.ad_id));
-
+					if(ad == undefined){
+						console.log('error - invalid ad id');
+						res.json({message: 'error -- invalid ad id'});
+					} else{
+						//return campaign with specific id
+						console.log('getting ad with id ' + req.params.ad_id);
+						res.json(ad);
+					}
 				}
-
 			}
-				
 		});
 	})
 
@@ -444,15 +500,18 @@ router.route('/mediabuyers/:mediabuyer_id/campaigns/:campaign_id/ads/:ad_id')
 				console.log("Not a valid mediabuyer, campaign, or ad id");
 			} else{
 
-				//grab our campaign
+				//grab the actual campiagn object out of the populated array
 				campaign = mediabuyer.campaigns[0];
 
-				//check campaign validity
+				//ensure campaign with id :campiagn_id exists
+				//if mediabuyer has no campaigns created, the returned object could be null
+				//population would have no effect in this scenario, but embedded objects are still accessible
 				if(campaign == undefined){
 					console.log('error -- undefined campaign');
 					res.json({message: 'error -- invalid campaign id'});
 				} else{
 
+					//store the ad with id :ad_id
 					ad = campaign.ads.id(req.params.ad_id);
 
 					//check ad validity
@@ -463,9 +522,11 @@ router.route('/mediabuyers/:mediabuyer_id/campaigns/:campaign_id/ads/:ad_id')
 
 						//change your ad status if desired
 						ad.status = req.body.status;
+
 						//set bidding values
 						ad.bid.bidtype = req.body.bidtype;
 						ad.bid.amount = req.body.amount;
+
 						//set targeting spec
 						ad.target.venue_name = req.body.venue_name;
 						ad.target.venue_city = req.body.venue_city;
@@ -478,6 +539,7 @@ router.route('/mediabuyers/:mediabuyer_id/campaigns/:campaign_id/ads/:ad_id')
 						//save campaign object
 						campaign.save(function(err){
 
+							//ensure validity of ad attributes
 							if (err) {
 								res.send(err);
 								console.log('error updating ad');
@@ -486,23 +548,11 @@ router.route('/mediabuyers/:mediabuyer_id/campaigns/:campaign_id/ads/:ad_id')
 								console.log('updating advertisement');
 								res.json({message: "Ad has been updated"});
 							}
-							
 						});
-
-
 					}
-
-					
-
-
 				}
-
-				
-
 			}
-
 		});
-
 	});
 
 
